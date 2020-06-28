027dbbd432e7301a669caa30803b523f3a6ec657
deepak bhatt <deepakbhattmits@gmail.com>
2020-06-28 12:20:59 +0530

    refactor

---
+++ src/Button.jsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from 'react';
+const Button = ({ className, handleClick, children }) => {
+	return (
+		<>
+			<button className={className} onClick={handleClick}>
+				{children}
+			</button>
+		</>
+	);
+};
+export default Button;
