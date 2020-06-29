3fc3e1e1e5422247dcd2be70e836dae3bf478e3a
deepak bhatt <deepakbhattmits@gmail.com>
2020-06-29 22:13:12 +0530

    refacfor

--- src/App.test.js
+++ src/App.test.js
@@ -4,8 +4,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 // default test case
-test('renders learn react link', () => {
-	const { getByText } = render(<App />);
-	const linkElement = getByText(/learn react/i);
-	expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+// 	const { getByText } = render(<App />);
+// 	const linkElement = getByText(/learn react/i);
+// 	expect(linkElement).toBeInTheDocument();
+// });
