d6a260849f74f0e72054d370d9d545d579a6ad76
deepakbhattmits <deepakbhattmits@gmail.com>
2022-06-14 20:21:36 +0530

    Create Button.jsx

---
+++ src_BK/components/reusable/Button.jsx
@@ -0,0 +1,9 @@
+/** @format */
+const Button = ({ className, handleClick, children }) => {
+	return (
+		<button className={className} onClick={handleClick}>
+			{children}
+		</button>
+	);
+};
+export default Button;
