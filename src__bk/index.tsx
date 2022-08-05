7b16bca6b82fded1936cbe3921072360576d1a02
deepakbhattmits <deepakbhattmits@gmail.com>
2022-08-05 16:44:10 +0530

    Update index.tsx

--- src__bk/index.tsx
+++ src__bk/index.tsx
@@ -1,27 +1,33 @@
 /** @format */
-
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles/index.css";
-// import App from './components/App';
+// import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Hydrate } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import App from "./components/App";
 import Pokemon from "./components/Pokemon";
 // import * as serviceWorker from './serviceWorker';
-import { QueryClient, QueryClientProvider } from "react-query";
-import { Hydrate } from "react-query/hydration";
-import { ReactQueryDevtools } from "react-query/devtools";
+import "./styles/_index.scss";
+// import { ReactNode } from "react";
+
+// declare module "react-query/types/react/QueryClientProvider" {
+//   interface QueryClientProviderProps {
+//     children?: ReactNode;
+//   }
+// }
 const queryClient = new QueryClient();
 const rootElement = document.querySelector("#root");
-ReactDOM.render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Hydrate>
-        {/* <App /> */}
+const root = createRoot(rootElement);
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <Hydrate>
+      <main>
+        <App />
         <Pokemon />
         <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
-      </Hydrate>
-    </QueryClientProvider>
-  </React.StrictMode>,
-  rootElement
+      </main>
+    </Hydrate>
+  </QueryClientProvider>
 );
 
 // If you want your app to work offline and load faster, you can change
